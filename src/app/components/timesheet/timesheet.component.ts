807dd64995811d5c29abd9cceabb0193096519b2
Terran Adams <terranadams@Terrans-MBP.veracitynetworks.net>
2021-03-19 09:51:31 -0600

    unit 8

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -5,7 +5,6 @@ import { Department } from 'src/app/interfaces/department';
 import { FormControl, ValidatorFn, AbstractControl } from '@angular/forms';
 import { Employee } from 'src/app/interfaces/employee';
 
-
 @Component({
   selector: 'app-timesheet',
   templateUrl: './timesheet.component.html',
@@ -17,6 +16,15 @@ export class TimesheetComponent implements OnInit {
   employeeNameFC = new FormControl('', this.nameValidator());
   employees: Employee[] = [];
   employeeId = 0;
+  weekdays: string[] = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday',
+  ];
 
   constructor(
     private route: ActivatedRoute,
@@ -39,6 +47,13 @@ export class TimesheetComponent implements OnInit {
         departmentId: this.department.id,
         name: this.employeeNameFC.value,
         payRate: Math.floor(Math.random() * 50) + 50,
+        monday: 0,
+        tuesday: 0,
+        wednesday: 0,
+        thursday: 0,
+        friday: 0,
+        saturday: 0,
+        sunday: 0,
       });
 
       this.employeeNameFC.setValue('');
@@ -47,15 +62,31 @@ export class TimesheetComponent implements OnInit {
 
   nameValidator(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
-        let error = null;
-        if (this.employees && this.employees.length) {
-            this.employees.forEach(employee => {
-                if (employee.name.toLowerCase() === control.value.toLowerCase()) {
-                    error = {duplicate: true};
-                }
-            });
-        }
-        return error;
+      let error = null;
+      if (this.employees && this.employees.length) {
+        this.employees.forEach((employee) => {
+          if (employee.name.toLowerCase() === control.value.toLowerCase()) {
+            error = { duplicate: true };
+          }
+        });
+      }
+      return error;
     };
-}
+  }
+
+  getTotalHours(employee: Employee): number {
+    return (
+      employee.monday +
+      employee.tuesday +
+      employee.wednesday +
+      employee.thursday +
+      employee.friday +
+      employee.saturday +
+      employee.sunday
+    );
+  }
+
+  deleteEmployee(index: number): void {
+    this.employees.splice(index, 1);
+  }
 }
--- src/app/interfaces/employee.ts
+++ src/app/interfaces/employee.ts
@@ -3,4 +3,11 @@ export interface Employee {
   departmentId: string;
   name: string;
   payRate: number;
+  monday: number;
+  tuesday: number;
+  wednesday: number;
+  thursday: number;
+  friday: number;
+  saturday: number;
+  sunday: number;
 }
