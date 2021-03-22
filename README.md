c93b1c38fa2f77d1edeef3350e010427f5e13d28
Terran Adams <terranadams@Terrans-MacBook-Pro.local>
2021-03-22 11:55:15 -0600

    unit 12

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # HrTimesheet
-
+https://github.com/MountainlandWEB/angular/tree/master/timesheet_app_walkthrough
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.2.
 
 ## Development server
--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DepartmentsService } from '../../services/departments.service';
 import { Department } from 'src/app/interfaces/department';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-departments',
@@ -9,7 +11,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./departments.component.scss'],
 })
 export class DepartmentsComponent implements OnInit {
-  departments: Department[]
+  departments: Department[];
+  $departments: Observable<Department[]>;
 
   constructor(
     private departmentsService: DepartmentsService,
@@ -17,9 +20,11 @@ export class DepartmentsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.departmentsService.getDeparments().subscribe(departments => {
-      this.departments = departments;
-  });  }
+  //   this.departmentsService.getDeparments().subscribe(departments => {
+  //     this.departments = departments;
+  // });  
+  this.$departments = this.departmentsService.getDeparments();
+}
 
   goToDepartment(departmentId: string): void {
     this.router.navigate(['./timesheet', { id: departmentId }]);
