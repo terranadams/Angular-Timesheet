c4f3903735cf09609d5075a0e72ff42e6fe00e61
Terran Adams <terranadams@Terrans-MacBook-Pro.local>
2021-03-22 08:49:52 -0600

    unit 9

--- src/app/components/asdf.js
+++
@@ -1,8 +0,0 @@
-function yoMama() {
-  console.log("hi");
-}
-let wuddup = "wuddup";
-
-if (wuddup) {
-  console.log(wuddup);
-}
--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -8,6 +8,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [],
@@ -21,6 +22,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatFormFieldModule,
     ReactiveFormsModule,
+    MatTabsModule
   ],
   exports: [
     MatToolbarModule,
@@ -31,6 +33,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatFormFieldModule,
     ReactiveFormsModule,
+    MatTabsModule
   ],
 })
 export class MaterialModule {}
